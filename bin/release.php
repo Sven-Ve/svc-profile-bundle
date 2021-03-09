34ab8794396c59ce741c31d393c427017d37c2f2
Sven Vetter <git.09o@svenvetter.com>
2021-03-09 22:48:20 +0100

    sign official commits and tags

--- bin/release.php
+++ bin/release.php
@@ -7,10 +7,10 @@ $message = "added captcha to change profile/password";
 file_put_contents("README.md", "\n* Version " . $version . ": " . $message, FILE_APPEND);
 
 $res = shell_exec('git add .');
-$res = shell_exec('git commit -m "' . $message . '"');
+$res = shell_exec('git commit -S -m "' . $message . '"');
 $res = shell_exec('git push');
 
-$res = shell_exec('git tag -a ' . $version . ' -m "' . $message . '"');
+$res = shell_exec('git tag -a -s ' . $version . ' -m "' . $message . '"');
 $res = shell_exec('git push origin ' . $version);
 
 $res = shell_exec('ssh svenvett@svenvett.myhostpoint.ch  "cd /home/svenvett/www/satis; bin/satis build satis.json"');
